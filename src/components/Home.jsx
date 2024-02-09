ed86bade84034cc3a54bc51e70303372ab515919
Young <youngheesomemoto@gmail.com>
2024-02-08 22:05:42 -0600

    fancy home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,14 @@
 export default function Home() {
     return (
         <div className="home">
-            <h1>HOME</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum cumque enim nisi saepe repudiandae quo, fuga, odio aliquid maxime neque ex quidem, 
+                itaque dolorum ea ipsam quis reprehenderit vero suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita voluptatem cupiditate iste qui libero magnam cumque, reiciendis esse recusandae tempora saepe 
+                culpa accusamus odio doloremque harum ipsam corporis rem eaque?Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, alias distinctio perferendis, incidunt repellat assumenda aut nihil voluptatum sunt ducimus dolorem beatae? Excepturi, 
+                odit temporibus mollitia ipsa impedit ipsam dignissimos! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima cumque quaerat maiores possimus, fugit et laudantium ipsa voluptatum
+                 error nemo placeat maxime dolorem mollitia officiis doloribus porro laboriosam quasi repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, laboriosam. Excepturi corrupti eligendi quae quis itaque qui odit pariatur facere fugiat nam, 
+                 ipsa debitis repellat ipsum minus vitae soluta aperiam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati provident facilis reprehenderit necessitatibus optio illo quaerat dolores minus dolore quis quam perspiciatis exercitationem suscipit 
+                 magnam nihil, expedita repellat impedit illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi suscipit repudiandae deleniti facilis, reiciendis, velit repellendus cum fuga ullam, dolorem placeat voluptatum id ex fugit ipsum magni officiis
+                  modi voluptate.</h1>
         </div>
     );
 }
